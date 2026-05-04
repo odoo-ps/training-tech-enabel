91ee5f29fee53e8fd14223496273c3d6cefc5b4c
umbm-odoo <umbm@odoo.com>
2026-05-04 15:35:10 +0200

    [ADD] new module boilerplate

---
+++ odoo-trainings/kawiil_financing/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Kawiil Financing',
+    'summary': 'Streamlines the loan application process for dealerships.',
+    'license': 'OPL-1',
+    'category': 'Kawiil/Kawiil',
+    'author': 'Odoo, Inc.',
+    'website': 'https://github.com/odoo-trainings/kawiil-base/',
+    'version': '1.0.0',
+    'depends': ['base'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'application': True,
+} 
