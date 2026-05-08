81cf4e92a8f433514432b3e5afdeae09cae2781a
Stéphane Delbeke <stephane.delbeke@enabel.be>
2026-05-08 14:43:44 +0200

    add manifest

--- kawiil_financing/models/__init__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Kawiil Financing',
-    'version': '19.0.0',
-    'category': 'Kawill/Custom Modules',
-    'summary': 'Simplifie le processus de demande de prêt pour les concessionnaires',
-    'author': 'sdelbeke',
-    'license': 'OPL-1',
-    'application': True,
-}
