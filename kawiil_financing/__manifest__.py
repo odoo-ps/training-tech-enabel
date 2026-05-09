6f70d39b9eb8fa8221b60eb8b3746a5ee37d11f7
Malick SENE <malick.sene@enabel.be>
2026-05-09 11:51:50 +0000

    [ADD]: [2.01] initialisation de  kawiil_financing.

---
+++ kawiil_financing/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Kawiil Financing",
+    'summary': "Simplifie le processus de demande de prêt pour les concessionnaires.",
+    'category': "Kawiil/Custom Modules",
+    'version': "1.0.0",
+    'author': "Malick SENE/MalickSe",
+    'depends': ['base'],
+    'license': "OPL-1",
+    'application': True,
+}
