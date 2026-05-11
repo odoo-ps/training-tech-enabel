1e9752c153dd6643ab799b651472c4ba45448b6c
malorie <malorie.amoussou@enabel.be>
2026-05-11 21:40:19 +0100

    [2.01] initialize kawiil_financing manifest

---
+++ src/user/kawiil_financing/__manifest__.py
@@ -0,0 +1,11 @@
+
+{
+    "name": "Kawiil Financing",
+    "summary": "Simplifie le processus de demande de prêt pour les concessionnaires.",
+    "category": "Kawiil/Custom Modules",
+    "version": "1.0.0",
+    "author": "Malorie Amoussou",
+    "depends": ["base"],
+    "license": "OPL-1",
+    "application": True,
+}
