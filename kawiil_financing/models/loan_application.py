245478919926b0ed3d1fc82194129832938c0989
umbm-odoo <umbm@odoo.com>
2026-05-18 11:28:35 +0200

    [IMP] kawiil_financing: add sql and python constraints to loan application

--- kawiil_financing/models/loan_application.py
+++ kawiil_financing/models/loan_application.py
@@ -1,9 +1,21 @@
-from odoo import models, fields
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class LoanApplication(models.Model):
     _name = 'loan.application'
     _description = 'Loan Application'
+
+    _name_uniq = models.Constraint(
+        'unique(name)',
+        'A tag with the same name already exists in this country.',
+    )
+
+    _principal_amount_positive_check = models.Constraint(
+        "CHECK(principal_amount >= 0)",
+        "The principal amount must be a positive value.",
+    )
+
 
     name = fields.Char(string="Numéro de demande", required=True)
 
@@ -21,7 +33,10 @@ class LoanApplication(models.Model):
 
     loan_amount = fields.Monetary(
         string="Montant du prêt",
-        required=True
+        compute='_compute_loan_amount',
+        inverse='_inverse_loan_amount',
+        store=True,
+        currency_field='currency_id'
     )
 
     currency_id = fields.Many2one(
@@ -60,8 +75,9 @@ class LoanApplication(models.Model):
     )
 
     down_payment = fields.Monetary(
-    string="Apport initial",
-    currency_field='currency_id'
+        string="Apport initial",
+        currency_field='currency_id',
+        default=0.0
     )
 
     date_applied = fields.Date(
@@ -75,6 +91,30 @@ class LoanApplication(models.Model):
     notes = fields.Html(
         string="Notes internes"
     )
+
+    customer_email = fields.Char(related='partner_id.email')
+    customer_phone = fields.Char(related='partner_id.phone')
+    principal_amount = fields.Monetary(
+        string="Montant principal",
+        currency_field='currency_id',
+        required=True
+    )
+
+    @api.constrains('principal_amount', 'down_payment')
+    def _check_amounts(self):
+        for rec in self:
+            if rec.down_payment >= rec.principal_amount:
+                raise ValidationError(_("The initial payment should be less than the total amount."))
+
+    @api.depends('principal_amount', 'down_payment')
+    def _compute_loan_amount(self):
+        for rec in self:
+            rec.loan_amount = (rec.principal_amount or 0.0) - (rec.down_payment or 0.0)
+
+    def _inverse_loan_amount(self):
+        for rec in self:
+            rec.down_payment = (rec.principal_amount or 0.0) - (rec.loan_amount or 0.0)
+
 
 #  TAGS
 class LoanApplicationTag(models.Model):
